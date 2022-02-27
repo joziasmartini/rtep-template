0b3217a8e1abcf20ac3e8ab066a04a72ca321b28
Jozias Martini <jozias.martini@gmail.com>
2022-02-27 00:23:09 -0300

    Update project structure

---
+++ src/pages/Index/Index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Index.css";
+
+function Index() {
+  return (
+    <div className="Index">
+      <h1>This is the first page.</h1>
+    </div>
+  );
+}
+
+export default Index;
